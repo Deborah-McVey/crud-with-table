6211e523489f37604179de82a66f99830c9a56f9
Deb <139592532+Deborah-McVey@users.noreply.github.com>
2023-12-04 20:43:44 -0500

    got bugs out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { CdkTableModule } from '@angular/cdk/table';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { PersonFormDialogComponent } from 'src/app/components/person-form-dialog/person-form-dialog.component';
+import { DataTableComponent } from 'src/app/components/data-table/data-table.component';
+import { ConfirmationDialogComponent } from 'src/app/components/confirmation-dialog/confirmation-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonFormDialogComponent,
+    DataTableComponent,
+    ConfirmationDialogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CdkTableModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  entryComponents: [ConfirmationDialogComponent]
+}
---
+++ src/app/core/models/persons.ts
@@ -0,0 +1,13 @@
+export class Person {
+  id?: number;
+  firstName: string;
+  age: number;
+  job: string;
+
+  constructor(id: number = null, firstName: string = '', age: number = 0, job: string = '') {
+    this.id = id;
+    this.firstName = firstName;
+   this.age = age;
+   this.job = job;
+ }
+}
